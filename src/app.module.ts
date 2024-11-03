e9ee9497f0b4a47037e5231025e946c46139afad
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-03 21:55:20 +0700

    feat : import province sercice, traditional service to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TraditionalClothingModule } from './traditional-clothing/traditional-clothing.module';
+import { ProvinceService } from './region/province.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, TraditionalClothingModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProvinceService],
 })
 export class AppModule {}

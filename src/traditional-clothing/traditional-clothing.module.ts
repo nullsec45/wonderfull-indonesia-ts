402b9c6cb610f4b9c13a5632662749c100961d2d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-05 23:49:05 +0700

    feat/traditional-clothing : register library multer

--- src/traditional-clothing/traditional-clothing.module.ts
+++ src/traditional-clothing/traditional-clothing.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { TraditionalClothingService } from './traditional-clothing.service';
 import { TraditionalClothingController } from './traditional-clothing.controller';
 import { PrismaService } from 'services/prisma.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { FileUploadService } from 'services/file-upload/file-upload.service';
 
 @Module({
+  imports:[
+    MulterModule.register({
+      storage:diskStorage({
+        destination:'./uploads/traditional-clothings',
+        filename:(req,file,cb) => {
+          const filename=`${Date.now()}_${file.originalname}`;
+          cb(null, filename);
+        }
+      })
+    })
+  ],
   controllers: [TraditionalClothingController],
-  providers: [TraditionalClothingService,PrismaService],
+  providers: [TraditionalClothingService,PrismaService, FileUploadService],
 })
 export class TraditionalClothingModule {}

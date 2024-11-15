97956a62716e635866cc35338c22507669f59fde
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-15 22:10:25 +0700

    feat(traditional-clothing) : register multer to module.

--- src/traditional-clothing/traditional-clothing.module.ts
+++ src/traditional-clothing/traditional-clothing.module.ts
@@ -5,16 +5,22 @@ import { PrismaService } from 'services/prisma.service';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FileUploadService } from 'services/file-upload/file-upload.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports:[
-    MulterModule.register({
-      storage:diskStorage({
-        destination:'./uploads/traditional-clothings',
-        filename:(req,file,cb) => {
-          const filename=`${Date.now()}_${file.originalname}`;
-          cb(null, filename);
-        }
+    ConfigModule,
+    MulterModule.registerAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:(configService:ConfigService) => ({
+          storage:diskStorage({
+          destination:configService.get<string>('PATH_FILE'),
+            filename:(req,file,cb) => {
+              const filename=`${Date.now()}_${file.originalname}`;
+              cb(null, filename);
+            }
+          })
       })
     })
   ],

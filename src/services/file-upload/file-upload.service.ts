32152138274d6b4267fc7211cde65a519f13da10
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-15 22:08:31 +0700

    feat(file-upload) : create methot handle file delete.

--- src/services/file-upload/file-upload.service.ts
+++ src/services/file-upload/file-upload.service.ts
@@ -1,10 +1,16 @@
 import { BadRequestException, Injectable, HttpStatus } from '@nestjs/common';
 import { ResponseData } from 'types/response';
 import {responseValue, responseValueWithData, } from 'utils/response';
-
+import * as fs from 'fs';
+import * as path from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FileUploadService {
+    constructor(private configService:ConfigService){
+
+    }
+    
     async handleFileUpload(file:Express.Multer.File):Promise<ResponseData>{
         try{
             if(!file){
@@ -22,6 +28,20 @@ export class FileUploadService {
             }
 
             return responseValueWithData(true, HttpStatus.OK, "File uploaded successfully", { data: file.path });
+        }catch(error){
+            return responseValue(false, HttpStatus.CONFLICT, error.message);
+        }
+    }
+
+    async handleFileDelete(fileName:string){
+        try{
+            // console.log(fileName);
+            // const filePath = path.join(this.configService.get<string>('PATH_FILE'), fileName);
+            if (fs.existsSync(fileName)) {
+                fs.unlinkSync(fileName); // Menghapus file
+            } else {
+                return responseValue(false, HttpStatus.NOT_FOUND, "File Not Found");
+            }
         }catch(error){
             return responseValue(false, HttpStatus.CONFLICT, error.message);
         }

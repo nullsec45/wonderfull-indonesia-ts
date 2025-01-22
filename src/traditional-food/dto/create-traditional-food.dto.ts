c788d47aec8f50efb36e3b052a42832d81b26dc9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-22 16:10:14 +0700

    documentation : create documentation API traditional food.

--- src/traditional-food/dto/create-traditional-food.dto.ts
+++ src/traditional-food/dto/create-traditional-food.dto.ts
@@ -1,10 +1,31 @@
 import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
 
 export class CreateTraditionalFoodDto {
+    @ApiProperty({
+        example: 'Kerak Telor',
+        required: true,
+    })
     @IsNotEmpty()
     @IsString()
     name:string;
 
+    @ApiProperty({
+        type: 'string',
+        format: 'binary', 
+        description: 'PNG|JPEG',
+        required: true,
+        example: 'image.png',
+    })
+    @IsNotEmpty()
+    @IsString()
+    file:string;
+
+    @ApiProperty({
+        example:  1,
+        required: true,
+    })
     @IsNotEmpty()
     @IsString()
     province_id:string;
--- src/traditional-food/traditional-food.controller.ts
+++ src/traditional-food/traditional-food.controller.ts
@@ -18,7 +18,9 @@ import { UpdateTraditionalFoodDto } from './dto/update-traditional-food.dto';
 import { PaginationDTO } from 'dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from 'services/file-upload/file-upload.service';
+import { ApiTags, ApiResponse, ApiBody, ApiOperation,ApiConsumes } from '@nestjs/swagger'
 
+@ApiTags('Traditional Food')
 @Controller('traditional-food')
 export class TraditionalFoodController {
   constructor(
@@ -26,7 +28,29 @@ export class TraditionalFoodController {
     private readonly fileUploadService : FileUploadService
   ) {}
 
+  @ApiBody({
+    description: 'request body post traditional food.',
+    type: CreateTraditionalFoodDto
+  })
   @Post()
+  @ApiOperation({ summary: 'Submit form with multipart/form-data' })
+  @ApiConsumes('multipart/form-data')
+  @ApiResponse({
+    status: 201,
+    description: 'Success Post Traditional Food.'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Province Id.'
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'File Not Found.'
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Traditional food already exist.'
+  })
   @UseInterceptors(FileInterceptor('file'))
   async create(
     @Body() createTraditionalFoodDto: CreateTraditionalFoodDto,
@@ -43,18 +67,48 @@ export class TraditionalFoodController {
     return response.status(createTraditionalFood.statusCode).json(createTraditionalFood)
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Get all data traditional house.'
+  })
   @Get()
   async findAll(@Query() paginationDTO: PaginationDTO, @Res() response:Response) {
     const traditionalFoods=await this.traditionalFoodService.findAll(paginationDTO);
     return response.status(traditionalFoods.statusCode).json(traditionalFoods);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Get data traditional house by id.'
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.traditionalFoodService.findOne(id);
   }
 
+   @ApiBody({
+        description: 'request body update traditional house.',
+        type: CreateTraditionalFoodDto
+  })
   @Patch(':id')
+  @ApiOperation({ summary: 'Submit form with multipart/form-data' })
+  @ApiConsumes('multipart/form-data')
+  @ApiResponse({
+    status: 200,
+    description: 'Success Patch Traditional Food.'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Province Id.'
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'File Not Found.'
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Traditional food already exist.'
+  })
   @UseInterceptors(FileInterceptor('file'))
   async update(
     @Param('id') id: string, 
@@ -78,6 +132,10 @@ export class TraditionalFoodController {
   }
 
   @Delete(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Success Delete Traditional Food.'
+  })
   async remove(@Param('id') id: string, @Res() response:Response) {
     const deleteTraditionalFood= await  this.traditionalFoodService.remove(id);
     return response.status(deleteTraditionalFood.statusCode).json(deleteTraditionalFood);

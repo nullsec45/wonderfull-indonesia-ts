215dbc4830242676ee1f487582e5611b199df860
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-19 17:36:16 +0700

    faet : set swagger only to dev * use global pipe validation.

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v1')
+  app.setGlobalPrefix('v1')
 
-  const config = new DocumentBuilder()
-    .setTitle('Wonderfull Indonesia API')
-    .setDescription('This is an API about diversity and everything about Indonesia')
-    .setVersion('1.0')
-    .addTag('wonderfull')
-    .build();
+  if (process.env.APP_ENV !== 'production'){
+    const config = new DocumentBuilder()
+                      .setTitle('Wonderfull Indonesia API')
+                      .setDescription('This is an API about diversity and everything about Indonesia')
+                      .setVersion('1.0')
+                      .addTag('wonderfull')
+                      .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs-api', app, documentFactory);
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs-api', app, documentFactory);
+  }
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+  }));
+
 
   await app.listen(3000);
 }
